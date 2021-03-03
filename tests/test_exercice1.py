b158d59d8230e4a2f517fac5599573c4f0d621c3
ValMssx <67736939+ValMssx@users.noreply.github.com>
2021-03-03 15:06:39 +0100

    Update test_exercice1.py

--- tests/test_exercice1.py
+++ tests/test_exercice1.py
@@ -1,8 +1,7 @@
 from exercices.exercice1 import *
 
-def test_mini():
-    t_moy = [14.9, 13.3, 13.1, 12.5, 13.0, 13.6, 13.7]
-    annees = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
-    resultat = mini(t_moy,annees) 
-    assert resultat  == (12.5, 2016) 
-
+def test_recherche_elt():
+    assert recherche(1, [2, 3, 4]) == -1
+    assert recherche(1, [10, 12, 1, 56]) == 2
+    assert recherche(50, [1, 50, 1]) == 1
+    assert recherche(15, [8, 9, 10, 15]) == 3
