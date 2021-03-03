07def2a71814e1b941b11f02fab3b81b5fb47eff
ValMssx <67736939+ValMssx@users.noreply.github.com>
2021-03-03 18:16:58 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,13 +1,13 @@
 from exercices.exercice2 import *
 
-def test_inversion():
+def test_inverse_chaine():
     chaine='bac'
     assert inverse_chaine(chaine) == 'cab'
     
-def test_palindrome():
+def test_est_palindrome():
     chaine='NSI'
     assert est_palindrome(chaine) ==False
 
-def test_nbre_palindrome():
+def test_est_nbre_palindrome():
     assert est_nbre_palindrome(214312) == False
     assert est_nbre_palindrome(213312) == True
